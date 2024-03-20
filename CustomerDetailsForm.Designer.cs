c04968da9f2e0af77c1588c8aa47959b3b872d44
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2024-03-21 00:16:21 +0530

    Updated CMA

--- CustomerDetailsForm.Designer.cs
+++ CustomerDetailsForm.Designer.cs
@@ -35,7 +35,7 @@ namespace CustomerManagementApp
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-          
+
             this.btnSave.Location = new System.Drawing.Point(26, 230);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 42);
@@ -43,7 +43,7 @@ namespace CustomerManagementApp
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            
+
             this.btnCancel.Location = new System.Drawing.Point(235, 294);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 48);
@@ -51,84 +51,84 @@ namespace CustomerManagementApp
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            
+
             this.lblName.AutoSize = true;
             this.lblName.Location = new System.Drawing.Point(13, 13);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(47, 16);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Name:";
-           
+
             this.lblEmail.AutoSize = true;
             this.lblEmail.Location = new System.Drawing.Point(13, 43);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(44, 16);
             this.lblEmail.TabIndex = 1;
             this.lblEmail.Text = "Email:";
-             
+
             this.lblPhone.AutoSize = true;
             this.lblPhone.Location = new System.Drawing.Point(13, 73);
             this.lblPhone.Name = "lblPhone";
             this.lblPhone.Size = new System.Drawing.Size(49, 16);
             this.lblPhone.TabIndex = 2;
             this.lblPhone.Text = "Phone:";
-             
+
             this.lblAddress.AutoSize = true;
             this.lblAddress.Location = new System.Drawing.Point(13, 103);
             this.lblAddress.Name = "lblAddress";
             this.lblAddress.Size = new System.Drawing.Size(61, 16);
             this.lblAddress.TabIndex = 3;
             this.lblAddress.Text = "Address:";
-            
+
             this.lblCompanyName.AutoSize = true;
             this.lblCompanyName.Location = new System.Drawing.Point(13, 133);
             this.lblCompanyName.Name = "lblCompanyName";
             this.lblCompanyName.Size = new System.Drawing.Size(108, 16);
             this.lblCompanyName.TabIndex = 4;
             this.lblCompanyName.Text = "Company Name:";
-             
+
             this.lblNotes.AutoSize = true;
             this.lblNotes.Location = new System.Drawing.Point(13, 163);
             this.lblNotes.Name = "lblNotes";
             this.lblNotes.Size = new System.Drawing.Size(46, 16);
             this.lblNotes.TabIndex = 5;
             this.lblNotes.Text = "Notes:";
-            
+
             this.txtName.Location = new System.Drawing.Point(85, 10);
             this.txtName.Name = "txtName";
             this.txtName.ReadOnly = true;
             this.txtName.Size = new System.Drawing.Size(250, 22);
             this.txtName.TabIndex = 6;
             this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
-            
+
             this.txtEmail.Location = new System.Drawing.Point(85, 40);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.ReadOnly = true;
             this.txtEmail.Size = new System.Drawing.Size(250, 22);
             this.txtEmail.TabIndex = 7;
             this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
-             
+
             this.txtPhone.Location = new System.Drawing.Point(85, 70);
             this.txtPhone.Name = "txtPhone";
             this.txtPhone.ReadOnly = true;
             this.txtPhone.Size = new System.Drawing.Size(250, 22);
             this.txtPhone.TabIndex = 8;
             this.txtPhone.TextChanged += new System.EventHandler(this.txtPhone_TextChanged);
-            
+
             this.txtAddress.Location = new System.Drawing.Point(85, 100);
             this.txtAddress.Name = "txtAddress";
             this.txtAddress.ReadOnly = true;
             this.txtAddress.Size = new System.Drawing.Size(250, 22);
             this.txtAddress.TabIndex = 9;
             this.txtAddress.TextChanged += new System.EventHandler(this.txtAddress_TextChanged);
-            
+
             this.txtCompanyName.Location = new System.Drawing.Point(130, 130);
             this.txtCompanyName.Name = "txtCompanyName";
             this.txtCompanyName.ReadOnly = true;
             this.txtCompanyName.Size = new System.Drawing.Size(205, 22);
             this.txtCompanyName.TabIndex = 10;
             this.txtCompanyName.TextChanged += new System.EventHandler(this.txtCompanyName_TextChanged);
-            
+
             this.txtNotes.Location = new System.Drawing.Point(85, 160);
             this.txtNotes.Multiline = true;
             this.txtNotes.Name = "txtNotes";
@@ -136,7 +136,7 @@ namespace CustomerManagementApp
             this.txtNotes.Size = new System.Drawing.Size(250, 60);
             this.txtNotes.TabIndex = 11;
             this.txtNotes.TextChanged += new System.EventHandler(this.txtNotes_TextChanged);
-             
+
             this.button1.Location = new System.Drawing.Point(130, 230);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 42);
@@ -144,7 +144,7 @@ namespace CustomerManagementApp
             this.button1.Text = "Clear";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            
+
             this.button2.Location = new System.Drawing.Point(45, 294);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(147, 48);
@@ -152,15 +152,23 @@ namespace CustomerManagementApp
             this.button2.Text = "Calculate Statistics";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            
+
+            this.lblCompanyName.AutoSize = true;
+            this.lblCompanyName.Location = new System.Drawing.Point(13, 133);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(107, 16);
+            this.lblCompanyName.TabIndex = 4;
+            this.lblCompanyName.Text = "Company Name";
+
             this.button3.Location = new System.Drawing.Point(235, 230);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 42);
             this.button3.TabIndex = 16;
-            this.button3.Text = "Undo";
+            this.button3.Text = "Undo Changes";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-             
+
+            // Reordering controls
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(347, 354);
@@ -186,7 +194,6 @@ namespace CustomerManagementApp
             this.Load += new System.EventHandler(this.CustomerDetailsForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         private System.Windows.Forms.Button btnSave;
--- CustomerDetailsForm.cs
+++ CustomerDetailsForm.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Windows.Forms;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Kernel.Pdf;
 
 namespace CustomerManagementApp
 {
@@ -36,36 +39,65 @@ namespace CustomerManagementApp
             customer.Notes = txtNotes.Text;
             MessageBox.Show("Changes saved successfully!");
 
-            
-            SaveCustomerDetailsToCSV(customer);
+            SaveCustomerDetailsToPDF(customer);
         }
 
-        private void SaveCustomerDetailsToCSV(Customer customer)
+        private void SaveCustomerDetailsToPDF(Customer customer)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
             saveFileDialog.Title = "Export Customer Details";
-            saveFileDialog.FileName = "customer_details.csv";
+            saveFileDialog.FileName = "customer_details.pdf";
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string exportFilePath = saveFileDialog.FileName;
 
-                using (var writer = new StreamWriter(exportFilePath))
+                using (FileStream fs = new FileStream(exportFilePath, FileMode.Create))
                 {
-                    writer.WriteLine("Name,Email,Phone,Address,CompanyName,Notes");
-                    string name = customer.Name ?? "";
-                    string email = customer.Email ?? "";
-                    string phone = customer.Phone ?? "";
-                    string address = customer.Address ?? "";
-                    string companyName = customer.CompanyName ?? "";
-                    string notes = customer.Notes ?? "";
+                    PdfWriter writer = new PdfWriter(fs);
+                    PdfDocument pdf = new PdfDocument(writer);
+                    Document document = new Document(pdf);
 
-                    writer.WriteLine($"{name},{email},{phone},{address},{companyName},{notes}");
+                    // Add metadata
+                    pdf.GetDocumentInfo().SetTitle("Customer Details");
+                    pdf.GetDocumentInfo().SetSubject("Exported Customer Details");
+                    pdf.GetDocumentInfo().SetCreator("Customer Management App");
+
+                    // Add header
+                    Paragraph header = new Paragraph("Customer Details\n\n");
+                    header.SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
+                    document.Add(header);
+
+                    // Add content
+                    Table table = new Table(2, false);
+                    table.SetHorizontalAlignment(iText.Layout.Properties.HorizontalAlignment.CENTER);
+
+                    AddCell(table, "Name");
+                    AddCell(table, customer.Name);
+                    AddCell(table, "Email");
+                    AddCell(table, customer.Email);
+                    AddCell(table, "Phone");
+                    AddCell(table, customer.Phone);
+                    AddCell(table, "Address");
+                    AddCell(table, customer.Address);
+                    AddCell(table, "Company Name");
+                    AddCell(table, customer.CompanyName);
+                    AddCell(table, "Notes");
+                    AddCell(table, customer.Notes);
+
+                    document.Add(table);
+                    document.Close();
                 }
 
                 MessageBox.Show($"Customer details exported successfully to:\n{exportFilePath}", "Export Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+        }
+
+        private void AddCell(Table table, string text)
+        {
+            Cell cell = new Cell().Add(new Paragraph(text));
+            table.AddCell(cell);
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
@@ -73,44 +105,8 @@ namespace CustomerManagementApp
             DisplayCustomerDetails(customer);
         }
 
-        private void txtName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtPhone_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtEmail_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtAddress_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtCompanyName_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtNotes_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void ClearTextBoxes()
         {
-
             previousData = new string[]
             {
                 txtName.Text,
@@ -120,7 +116,6 @@ namespace CustomerManagementApp
                 txtCompanyName.Text,
                 txtNotes.Text
             };
-
 
             txtName.Clear();
             txtEmail.Clear();
@@ -143,7 +138,6 @@ namespace CustomerManagementApp
 
         private void button3_Click(object sender, EventArgs e)
         {
-
             if (previousData != null && previousData.Length == 6)
             {
                 txtName.Text = previousData[0];
@@ -154,5 +148,14 @@ namespace CustomerManagementApp
                 txtNotes.Text = previousData[5];
             }
         }
+
+        private void txtName_TextChanged(object sender, EventArgs e) { }
+        private void txtPhone_TextChanged(object sender, EventArgs e) { }
+        private void txtEmail_TextChanged(object sender, EventArgs e) { }
+        private void txtAddress_TextChanged(object sender, EventArgs e) { }
+        private void txtCompanyName_TextChanged(object sender, EventArgs e) { }
+        private void txtNotes_TextChanged(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e) { }
+
     }
 }
