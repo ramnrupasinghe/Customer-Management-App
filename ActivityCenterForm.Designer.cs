92d3616969e41419e81551cd98326ee52c22f5d0
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2024-03-28 17:15:05 +0530

    Updated CMA

--- ActivityCenterForm.Designer.cs
+++ ActivityCenterForm.Designer.cs
@@ -28,11 +28,15 @@
             this.rbSortByEmail = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnSaveFilteredLogs = new System.Windows.Forms.Button();
+            this.chkFilterByDateRange = new System.Windows.Forms.CheckBox();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvActivityLogs)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvActivityLogs
-            // 
+           
             this.dgvActivityLogs.AllowUserToAddRows = false;
             this.dgvActivityLogs.AllowUserToDeleteRows = false;
             this.dgvActivityLogs.BackgroundColor = System.Drawing.SystemColors.Control;
@@ -43,52 +47,42 @@
             this.dgvActivityLogs.ReadOnly = true;
             this.dgvActivityLogs.RowHeadersWidth = 51;
             this.dgvActivityLogs.RowTemplate.Height = 24;
-            this.dgvActivityLogs.Size = new System.Drawing.Size(634, 356);
+            this.dgvActivityLogs.Size = new System.Drawing.Size(634, 460);
             this.dgvActivityLogs.TabIndex = 0;
             this.dgvActivityLogs.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvActivityLogs_CellContentClick);
-            // 
-            // button1
-            // 
+           
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button1.Location = new System.Drawing.Point(97, 280);
+            this.button1.Location = new System.Drawing.Point(55, 394);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 44);
             this.button1.TabIndex = 1;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
+          
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button2.Location = new System.Drawing.Point(468, 280);
+            this.button2.Location = new System.Drawing.Point(482, 394);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 44);
             this.button2.TabIndex = 2;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // lblSearch
-            // 
+           
             this.lblSearch.AutoSize = true;
             this.lblSearch.Location = new System.Drawing.Point(24, 171);
             this.lblSearch.Name = "lblSearch";
             this.lblSearch.Size = new System.Drawing.Size(53, 16);
             this.lblSearch.TabIndex = 6;
             this.lblSearch.Text = "Search:";
-            // 
-            // txtSearch
-            // 
+           
             this.txtSearch.Location = new System.Drawing.Point(97, 168);
             this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(205, 22);
             this.txtSearch.TabIndex = 7;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
-            // 
-            // button3
-            // 
+            
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.button3.Location = new System.Drawing.Point(503, 202);
             this.button3.Name = "button3";
@@ -97,20 +91,16 @@
             this.button3.Text = "Sort";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
+           
             this.button4.BackColor = System.Drawing.Color.Tomato;
-            this.button4.Location = new System.Drawing.Point(277, 280);
+            this.button4.Location = new System.Drawing.Point(269, 394);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 44);
             this.button4.TabIndex = 9;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // rbSortByEmail
-            // 
+            
             this.rbSortByEmail.AutoSize = true;
             this.rbSortByEmail.Location = new System.Drawing.Point(187, 223);
             this.rbSortByEmail.Name = "rbSortByEmail";
@@ -119,9 +109,7 @@
             this.rbSortByEmail.Text = "ActivityType ";
             this.rbSortByEmail.UseVisualStyleBackColor = true;
             this.rbSortByEmail.CheckedChanged += new System.EventHandler(this.rbSortByEmail_CheckedChanged);
-            // 
-            // radioButton1
-            // 
+          
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(55, 223);
             this.radioButton1.Name = "radioButton1";
@@ -130,9 +118,7 @@
             this.radioButton1.Text = "Name";
             this.radioButton1.UseVisualStyleBackColor = true;
             this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // radioButton2
-            // 
+           
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(332, 223);
             this.radioButton2.Name = "radioButton2";
@@ -141,12 +127,62 @@
             this.radioButton2.Text = "ActivityDateTime";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
-            // ActivityCenterForm
-            // 
+           
+            this.btnSaveFilteredLogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSaveFilteredLogs.Location = new System.Drawing.Point(503, 289);
+            this.btnSaveFilteredLogs.Name = "btnSaveFilteredLogs";
+            this.btnSaveFilteredLogs.Size = new System.Drawing.Size(110, 69);
+            this.btnSaveFilteredLogs.TabIndex = 13;
+            this.btnSaveFilteredLogs.Text = "Save Filtered";
+            this.btnSaveFilteredLogs.UseVisualStyleBackColor = false;
+            this.btnSaveFilteredLogs.Click += new System.EventHandler(this.btnSaveFilteredLogs_Click);
+             
+            this.chkFilterByDateRange.AutoSize = true;
+            this.chkFilterByDateRange.Location = new System.Drawing.Point(12, 326);
+            this.chkFilterByDateRange.Name = "chkFilterByDateRange";
+            this.chkFilterByDateRange.Size = new System.Drawing.Size(152, 20);
+            this.chkFilterByDateRange.TabIndex = 14;
+            this.chkFilterByDateRange.Text = "Filter by Date Range";
+            this.chkFilterByDateRange.UseVisualStyleBackColor = true;
+            this.chkFilterByDateRange.CheckedChanged += new System.EventHandler(this.chkFilterByDateRange_CheckedChanged);
+             
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(172, 322);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(154, 22);
+            this.dateTimePickerStartDate.TabIndex = 15;
+            this.dateTimePickerStartDate.Visible = false;
+            
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(332, 322);
+            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
+            this.dateTimePickerEndDate.Size = new System.Drawing.Size(148, 22);
+            this.dateTimePickerEndDate.TabIndex = 16;
+            this.dateTimePickerEndDate.Visible = false;
+            
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Location = new System.Drawing.Point(169, 289);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(69, 16);
+            this.lblStartDate.TabIndex = 17;
+            this.lblStartDate.Text = "Start Date:";
+            this.lblStartDate.Visible = false;
+           
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Location = new System.Drawing.Point(329, 289);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(66, 16);
+            this.lblEndDate.TabIndex = 18;
+            this.lblEndDate.Text = "End Date:";
+            this.lblEndDate.Visible = false;
+            
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 356);
+            this.ClientSize = new System.Drawing.Size(634, 460);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.dateTimePickerEndDate);
+            this.Controls.Add(this.dateTimePickerStartDate);
+            this.Controls.Add(this.chkFilterByDateRange);
+            this.Controls.Add(this.btnSaveFilteredLogs);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.rbSortByEmail);
@@ -177,5 +213,11 @@
         private System.Windows.Forms.RadioButton rbSortByEmail;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button btnSaveFilteredLogs;
+        private System.Windows.Forms.CheckBox chkFilterByDateRange;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEndDate;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.Label lblEndDate;
     }
 }
--- ActivityCenterForm.cs
+++ ActivityCenterForm.cs
@@ -39,11 +39,6 @@ namespace CustomerManagementApp
             }
         }
 
-        private void dgvActivityLogs_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-           
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             SaveActivityLogsToPdf();
@@ -79,14 +74,11 @@ namespace CustomerManagementApp
 
                     table.HeaderRows = 1;
 
-                    for (int i = 0; i < dgvActivityLogs.Rows.Count; i++)
+                    foreach (DataGridViewRow dgvRow in dgvActivityLogs.Rows)
                     {
-                        for (int j = 0; j < dgvActivityLogs.Columns.Count; j++)
+                        foreach (DataGridViewCell dgvCell in dgvRow.Cells)
                         {
-                            if (dgvActivityLogs.Rows[i].Cells[j].Value != null)
-                            {
-                                table.AddCell(new Phrase(dgvActivityLogs.Rows[i].Cells[j].Value.ToString()));
-                            }
+                            table.AddCell(new Phrase(dgvCell.Value?.ToString() ?? ""));
                         }
                     }
 
@@ -117,15 +109,6 @@ namespace CustomerManagementApp
             PopulateActivityLogGrid(filteredLogs);
         }
 
-        private void PopulateActivityLogGrid(List<ActivityLog> logs)
-        {
-            dgvActivityLogs.Rows.Clear();
-            foreach (ActivityLog log in logs)
-            {
-                dgvActivityLogs.Rows.Add(log.CustomerName, log.ActivityType, log.ActivityDateTime.ToString(), log.Details);
-            }
-        }
-
         private void button3_Click(object sender, EventArgs e)
         {
             string sortingCriteria = button3.Tag.ToString();
@@ -148,13 +131,61 @@ namespace CustomerManagementApp
 
         private void button4_Click(object sender, EventArgs e)
         {
-            
             foreach (DataGridViewRow row in dgvActivityLogs.SelectedRows)
             {
                 dgvActivityLogs.Rows.Remove(row);
             }
         }
 
+        private void chkFilterByDateRange_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePickerStartDate.Visible = chkFilterByDateRange.Checked;
+            dateTimePickerEndDate.Visible = chkFilterByDateRange.Checked;
+            lblStartDate.Visible = chkFilterByDateRange.Checked;
+            lblEndDate.Visible = chkFilterByDateRange.Checked;
+
+            if (!chkFilterByDateRange.Checked)
+            {
+                PopulateActivityLogGrid(activityLogs);
+            }
+        }
+
+        private void btnSaveFilteredLogs_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    if (chkFilterByDateRange.Checked)
+                    {
+                        DateTime startDate = dateTimePickerStartDate.Value;
+                        DateTime endDate = dateTimePickerEndDate.Value;
+
+                        filteredLogs = FilterLogsByDateRange(startDate, endDate);
+                    }
+                    else
+                    {
+                        filteredLogs = activityLogs;
+                    }
+
+                    SaveFilteredLogsToPdf(filteredLogs, filePath);
+                }
+            }
+        }
+        private void PopulateActivityLogGrid(List<ActivityLog> logs)
+        {
+            dgvActivityLogs.Rows.Clear();
+            foreach (ActivityLog log in logs)
+            {
+                dgvActivityLogs.Rows.Add(log.CustomerName, log.ActivityType, log.ActivityDateTime.ToString(), log.Details);
+            }
+        }
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             button3.Text = "Sort by Name";
@@ -171,8 +202,45 @@ namespace CustomerManagementApp
         {
             button3.Text = "Sort by ActivityDateTime";
 
-           
+
             button3.Tag = "ActivityDateTime";
+        }
+        private List<ActivityLog> FilterLogsByDateRange(DateTime startDate, DateTime endDate)
+        {
+            return activityLogs.Where(log => log.ActivityDateTime >= startDate && log.ActivityDateTime <= endDate).ToList();
+        }
+
+        private void SaveFilteredLogsToPdf(List<ActivityLog> filteredLogs, string filePath)
+        {
+            Document doc = new Document();
+            PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+            doc.Open();
+
+            PdfPTable table = new PdfPTable(dgvActivityLogs.Columns.Count);
+            for (int i = 0; i < dgvActivityLogs.Columns.Count; i++)
+            {
+                table.AddCell(new Phrase(dgvActivityLogs.Columns[i].HeaderText));
+            }
+
+            table.HeaderRows = 1;
+
+            foreach (ActivityLog log in filteredLogs)
+            {
+                table.AddCell(new Phrase(log.CustomerName));
+                table.AddCell(new Phrase(log.ActivityType));
+                table.AddCell(new Phrase(log.ActivityDateTime.ToString()));
+                table.AddCell(new Phrase(log.Details));
+            }
+
+            doc.Add(table);
+            doc.Close();
+
+            MessageBox.Show("Filtered activity logs saved to PDF successfully!");
+        }
+
+        private void dgvActivityLogs_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
