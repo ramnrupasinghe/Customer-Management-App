1c4376ad3f058addd577759cac8c6c94b933ec74
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2024-04-21 16:05:52 +0530

    Updated CMA

--- ChatForm.Designer.cs
+++ ChatForm.Designer.cs
@@ -23,8 +23,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.btnRecord = new System.Windows.Forms.Button();
             this.btnAttach = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            
+          
             this.lstChat.BackColor = System.Drawing.Color.LavenderBlush;
             this.lstChat.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.lstChat.FormattingEnabled = true;
@@ -32,24 +34,25 @@
             this.lstChat.Location = new System.Drawing.Point(16, 15);
             this.lstChat.Margin = new System.Windows.Forms.Padding(4);
             this.lstChat.Name = "lstChat";
-            this.lstChat.Size = new System.Drawing.Size(909, 484);
+            this.lstChat.Size = new System.Drawing.Size(780, 484);
             this.lstChat.TabIndex = 0;
             this.lstChat.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lstChat_DrawItem);
             this.lstChat.SelectedIndexChanged += new System.EventHandler(this.lstChat_SelectedIndexChanged);
-            
+          
             this.txtMessage.BackColor = System.Drawing.Color.Thistle;
-            this.txtMessage.Location = new System.Drawing.Point(16, 489);
+            this.txtMessage.Location = new System.Drawing.Point(16, 507);
             this.txtMessage.Margin = new System.Windows.Forms.Padding(4);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(591, 45);
+            this.txtMessage.Size = new System.Drawing.Size(371, 27);
             this.txtMessage.TabIndex = 1;
-            
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+           
             this.button1.BackColor = System.Drawing.Color.Plum;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(829, 489);
+            this.button1.Location = new System.Drawing.Point(674, 507);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 45);
+            this.button1.Size = new System.Drawing.Size(96, 34);
             this.button1.TabIndex = 2;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = false;
@@ -57,9 +60,9 @@
            
             this.btnRecord.BackColor = System.Drawing.Color.SkyBlue;
             this.btnRecord.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnRecord.Location = new System.Drawing.Point(625, 489);
+            this.btnRecord.Location = new System.Drawing.Point(406, 507);
             this.btnRecord.Name = "btnRecord";
-            this.btnRecord.Size = new System.Drawing.Size(96, 45);
+            this.btnRecord.Size = new System.Drawing.Size(96, 34);
             this.btnRecord.TabIndex = 3;
             this.btnRecord.Text = "Record";
             this.btnRecord.UseVisualStyleBackColor = false;
@@ -67,17 +70,35 @@
            
             this.btnAttach.BackColor = System.Drawing.Color.LightGreen;
             this.btnAttach.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAttach.Location = new System.Drawing.Point(727, 489);
+            this.btnAttach.Location = new System.Drawing.Point(540, 507);
             this.btnAttach.Name = "btnAttach";
-            this.btnAttach.Size = new System.Drawing.Size(96, 45);
+            this.btnAttach.Size = new System.Drawing.Size(96, 34);
             this.btnAttach.TabIndex = 4;
             this.btnAttach.Text = "Attach";
             this.btnAttach.UseVisualStyleBackColor = false;
             this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+           
+            this.button2.BackColor = System.Drawing.Color.SkyBlue;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(406, 565);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "filter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+         
+            this.txtSearch.BackColor = System.Drawing.Color.Thistle;
+            this.txtSearch.Location = new System.Drawing.Point(16, 571);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(371, 22);
+            this.txtSearch.TabIndex = 6;
             
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 566);
+            this.ClientSize = new System.Drawing.Size(818, 622);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.btnAttach);
             this.Controls.Add(this.btnRecord);
             this.Controls.Add(this.button1);
@@ -99,5 +120,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnRecord;
         private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSearch;
     }
 }
