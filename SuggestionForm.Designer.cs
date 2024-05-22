25d56de9e6cb294f1db78b68bc91a85577e8cb9f
ramnrupasinghe <ramnrupasinghe@students.nsbm.ac.lk>
2024-05-22 21:09:51 +0530

    Updated CMA

--- SuggestionForm.Designer.cs
+++ SuggestionForm.Designer.cs
@@ -20,35 +20,56 @@
             this.listBoxSuggestions = new System.Windows.Forms.ListBox();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.txtAutoSuggest = new System.Windows.Forms.TextBox();
+            this.lblRuleDetails = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            
+
+      
             this.listBoxSuggestions.FormattingEnabled = true;
             this.listBoxSuggestions.ItemHeight = 16;
-            this.listBoxSuggestions.Location = new System.Drawing.Point(12, 12);
+            this.listBoxSuggestions.Location = new System.Drawing.Point(12, 39);
             this.listBoxSuggestions.Name = "listBoxSuggestions";
-            this.listBoxSuggestions.Size = new System.Drawing.Size(776, 356);
+            this.listBoxSuggestions.Size = new System.Drawing.Size(776, 244);
             this.listBoxSuggestions.TabIndex = 0;
             this.listBoxSuggestions.SelectedIndexChanged += new System.EventHandler(this.listBoxSuggestions_SelectedIndexChanged);
-            this.btnOk.Location = new System.Drawing.Point(632, 384);
 
+           
+            this.btnOk.Location = new System.Drawing.Point(632, 307);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 38);
             this.btnOk.TabIndex = 1;
             this.btnOk.Text = "OK";
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
-           
-            this.btnCancel.Location = new System.Drawing.Point(713, 384);
+
+            this.btnCancel.Location = new System.Drawing.Point(713, 307);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 38);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+
+            this.txtAutoSuggest.Location = new System.Drawing.Point(12, 12);
+            this.txtAutoSuggest.Name = "txtAutoSuggest";
+            this.txtAutoSuggest.Size = new System.Drawing.Size(776, 22);
+            this.txtAutoSuggest.TabIndex = 3;
+            this.txtAutoSuggest.TextChanged += new System.EventHandler(this.txtAutoSuggest_TextChanged);
+
+            
+            this.lblRuleDetails.AutoSize = true;
+            this.lblRuleDetails.Location = new System.Drawing.Point(12, 307);
+            this.lblRuleDetails.Name = "lblRuleDetails";
+            this.lblRuleDetails.Size = new System.Drawing.Size(46, 17);
+            this.lblRuleDetails.TabIndex = 4;
+            this.lblRuleDetails.Text = "Details";
+
           
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRuleDetails);
+            this.Controls.Add(this.txtAutoSuggest);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.listBoxSuggestions);
@@ -56,6 +77,7 @@
             this.Text = "SuggestionForm";
             this.Load += new System.EventHandler(this.SuggestionForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -64,5 +86,7 @@
         private System.Windows.Forms.ListBox listBoxSuggestions;
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtAutoSuggest;
+        private System.Windows.Forms.Label lblRuleDetails;
     }
 }
--- SuggestionForm.cs
+++ SuggestionForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +16,14 @@ namespace CustomerManagementApp
         public string SelectedRule { get; private set; }
 
         private List<string> suggestedRules;
+        private Dictionary<string, string> ruleDetails;
 
         public SuggestionForm(List<string> rules)
         {
             InitializeComponent();
             suggestedRules = rules;
             PopulateSuggestions();
+            InitializeRuleDetails();
         }
 
         private void PopulateSuggestions()
@@ -28,6 +31,16 @@ namespace CustomerManagementApp
             foreach (var rule in suggestedRules)
             {
                 listBoxSuggestions.Items.Add(rule);
+            }
+        }
+
+        private void InitializeRuleDetails()
+        {
+            // Initialize rule details for demonstration purposes
+            ruleDetails = new Dictionary<string, string>();
+            foreach (var rule in suggestedRules)
+            {
+                ruleDetails[rule] = "Details about " + rule; // Placeholder for real details
             }
         }
 
@@ -52,15 +65,19 @@ namespace CustomerManagementApp
 
         private void SuggestionForm_Load(object sender, EventArgs e)
         {
-           
+
         }
 
         private void listBoxSuggestions_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (listBoxSuggestions.SelectedIndex != -1)
+            {
+                string selectedRule = listBoxSuggestions.SelectedItem.ToString();
+                lblRuleDetails.Text = ruleDetails[selectedRule];
+            }
         }
 
-       //highlight suggestion feature
+        // Highlight suggestion feature
         private void HighlightSuggestions(string keyword)
         {
             foreach (var item in listBoxSuggestions.Items)
@@ -73,12 +90,14 @@ namespace CustomerManagementApp
                 }
             }
         }
-        //sort suggestion
+
+        // Sort suggestion
         private void SortSuggestions()
         {
             listBoxSuggestions.Sorted = true;
         }
-        //filter the suggestions
+
+        // Filter the suggestions
         private void FilterSuggestions(string filter)
         {
             listBoxSuggestions.Items.Clear();
@@ -90,10 +109,27 @@ namespace CustomerManagementApp
                 }
             }
         }
-        //count suggestion feature
+
+        // Count suggestion feature
         private int CountSuggestions()
         {
             return listBoxSuggestions.Items.Count;
+        }
+
+        // New feature , Auto-Suggest Based on Typing
+        private void txtAutoSuggest_TextChanged(object sender, EventArgs e)
+        {
+            FilterSuggestions(txtAutoSuggest.Text);
+        }
+
+        // New feature ,Display Suggestion Details
+        private void listBoxSuggestions_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBoxSuggestions.SelectedIndex != -1)
+            {
+                string selectedRule = listBoxSuggestions.SelectedItem.ToString();
+                lblRuleDetails.Text = ruleDetails[selectedRule];
+            }
         }
     }
 }
